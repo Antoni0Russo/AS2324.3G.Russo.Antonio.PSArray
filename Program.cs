56fec7c9ab6a0c977669fc820a5823ac84ba12d3
Antonio Russo <Utente@LAPTOP-5C0S9O65>
2024-02-29 08:45:00 +0100

    2 feature : CaricaVettori

--- Program.cs
+++ Program.cs
@@ -9,12 +9,23 @@
             int nVoti= Convert.ToInt32(Console.ReadLine());
             int[] pesi = new int[nVoti];
             double[] voti = new double[nVoti];
+            StampaVotiPesi(pesi[], voti[],nVoti );
         }
+        //riguardare per probabile errore (voti e pesi non sono collegati)
         void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
         {
             for (int i = 0; i < nVoti; i++)
             {
                 Console.WriteLine("Voto: " + voti[i] + " - Peso: " + pesi[i]);
+            }
+        }
+        void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
+        {
+            Random random = new Random();
+            for (int i = 0;i < nVoti;i++)
+            {
+                pesi[i] = random.Next(1, 11);
+                voti[i] = random.Next(0, 101); 
             }
         }
 
