9855302f53450c9cc421ddc8cdc248dc0de277b9
Antonio Russo <Utente@LAPTOP-5C0S9O65>
2024-02-29 08:38:21 +0100

    1 feature : StampaVotiPesi

--- Program.cs
+++ Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("imposta quanti voti voi inserire  ");
+            int nVoti= Convert.ToInt32(Console.ReadLine());
+            int[] pesi = new int[nVoti];
+            double[] voti = new double[nVoti];
         }
+        void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        {
+            for (int i = 0; i < nVoti; i++)
+            {
+                Console.WriteLine("Voto: " + voti[i] + " - Peso: " + pesi[i]);
+            }
+        }
+
     }
 }
