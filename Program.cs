8605ce7186cdde1bbab8d5a08fb204cac9d06df2
Antonio Russo <Utente@LAPTOP-5C0S9O65>
2024-02-29 09:00:34 +0100

    3 feature : StampaVotiDispariMaggiori4 da fixare

--- Program.cs
+++ Program.cs
@@ -9,23 +9,46 @@
             int nVoti= Convert.ToInt32(Console.ReadLine());
             int[] pesi = new int[nVoti];
             double[] voti = new double[nVoti];
-            StampaVotiPesi(pesi[], voti[],nVoti );
+            StampaVotiPesi(voti, pesi, nVoti);
+            CaricaVettori(ref voti, ref pesi, nVoti);
+            Console.WriteLine("arrray caricato randomicamente ");
+            StampaVotiPesi(voti, pesi, nVoti);
+            StampaVotiDispariMaggiori4(ref voti, ref pesi, nVoti);
+            Console.WriteLine("array  ");
+            StampaVotiPesi(voti, pesi, nVoti);
+
         }
         //riguardare per probabile errore (voti e pesi non sono collegati)
-        void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
+        static void StampaVotiPesi(double[] voti, int[] pesi, int nVoti)
         {
             for (int i = 0; i < nVoti; i++)
             {
                 Console.WriteLine("Voto: " + voti[i] + " - Peso: " + pesi[i]);
             }
         }
-        void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
+        static void CaricaVettori(ref double[] voti, ref int[] pesi, int nVoti)
         {
             Random random = new Random();
-            for (int i = 0;i < nVoti;i++)
+            for (int i = 0; i < nVoti; i++)
             {
                 pesi[i] = random.Next(1, 11);
-                voti[i] = random.Next(0, 101); 
+                voti[i] = random.Next(0, 101);
+            }
+        }
+        //riguardare per probabile errore (non prende numeri dispari)
+        static void StampaVotiDispariMaggiori4(ref double[] voti, ref int[] pesi, int nVoti)
+        {
+            for (int i = 0; i < nVoti; i++)
+            {
+                if (voti[i] % 2 == 1)
+                {
+                    if (voti[i] >= 4)
+                    {
+                        Console.WriteLine("Voto: " + voti[i] + " - Peso: " + pesi[i]);
+                    }
+                }
+
+
             }
         }
 
